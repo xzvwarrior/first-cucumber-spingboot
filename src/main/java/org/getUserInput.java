3c5a015fdf18b4d1fa6817232cc5c9a0d113c390
xzvwarrior <dylan.trahan@outlook.com>
2022-08-14 13:16:43 -0500

    adding controllers

---
+++ src/main/java/org/getUserInput.java
@@ -0,0 +1,17 @@
+package org;
+
+import java.util.Scanner;
+
+//This is to allow the user to input their username and password. This is intended to be a stepping stone to eventually pull the credentials off of a JSON file and then encrypt the values.
+
+public class getUserInput {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter username");
+        String username = scan.nextLine();
+        System.out.printf(username);
+        System.out.println("Enter password");
+        String password = scan.nextLine();
+        System.out.printf(password);
+    }
+}
