cdfec79a22cae7318d5b1ee02d695fe42d173605
xzvwarrior <dylan.trahan@outlook.com>
2022-08-15 20:42:11 -0500

    Adding encryption

--- src/main/java/org/getUserInput.java
+++ src/main/java/org/getUserInput.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 //This is to allow the user to input their username and password. This is intended to be a stepping stone to eventually pull the credentials off of a JSON file and then encrypt the values.
 
 public class getUserInput {
-    public static void main(String[] args){
+
+    public static String getInputs() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter username");
         String username = scan.nextLine();
-        System.out.printf(username);
-        System.out.println("Enter password");
-        String password = scan.nextLine();
-        System.out.printf(password);
+//        System.out.printf(username);
+//        System.out.println("Enter password");
+//        String password = scan.nextLine();
+//        System.out.printf(password);
+        return username;
     }
 }
+
+
+
+
